b7dbc4972801824fd13742143cf4a5c41cfda0ed
ide Shouhi <shouhi0922@icloud.com>
2021-08-16 01:27:44 +0900

    :fire: remove-signup

--- lib/general_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-// firebaseAuthのインスタンスを提供
-final firebaseAuthProvider = Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
