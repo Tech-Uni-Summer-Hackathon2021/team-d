5943b9b4a3aa82675e2ff9b345dda201935c0922
yoshiki20010524 <75286616+yoshiki20010524@users.noreply.github.com>
2021-08-17 12:56:26 +0900

    new-posts-forms
    投稿フォームを作りました

--- lib/screens/form_questions.dart
+++ lib/screens/form_questions.dart
@@ -1,59 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:sawa/screens/main.dart';
 
 
-class NextPage extends StatelessWidget {
+class PostPage extends StatefulWidget {
 
   @override
+  _PostPagePageState createState() => _PostPagePageState();
+}
+
+class _PostPagePageState extends State<PostPage> {
+  _onSubmitted(String content){
+    CollectionReference posts = FirebaseFirestore.instance.collection('posts');
+    posts.add({
+      "content": content
+    });
+
+    /// 入力欄をクリアにする
+    _textEditingController.clear();
+    _textEditingControllerTitle.clear();
+  }
+  TextEditingController _textEditingController = TextEditingController();
+  TextEditingController _textEditingControllerTitle = TextEditingController();
+  @override
   final myFocusNode = FocusNode();
-
   Widget build(BuildContext context) {
-
-    final myController = TextEditingController();
     return Scaffold(
-        appBar: AppBar(
-          title: Text("投稿フォーム"),
-        ),
-        body: Container(
-          width: double.infinity,
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: '　質問タイトル',
+      appBar: AppBar(
+        title: Text("投稿フォーム"),
+      ),
+      body: Container(
+      width: double.infinity,
+    child: Column(
+        children:[
+          TextField(
+          controller: _textEditingControllerTitle,
+          onSubmitted: _onSubmitted,
+          enabled: true,
+          style: TextStyle(color: Colors.black),
+          obscureText: false,
+          maxLines:1 ,
+          decoration: const InputDecoration(
+            hintText: '　質問タイトル',
 
-                ),
-              ),
-
-              TextFormField( focusNode: myFocusNode,
-                maxLines: 15,
-                minLines: 15,
-                maxLength: 330,
-                style: TextStyle(fontSize: 18),
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
-                  hintText: '　内容',
-
-                ),
-                onChanged: (text) {
-
-                },
-              ),
-              RaisedButton(
-                child: Text('投稿'),
-                onPressed: () {
-                  final hobbyText = myController.text;
-                  Navigator.pop(
-                    context,
-                    MaterialPageRoute(builder: (context) => NextPage()),
-                  );
-                },
-              ),
-            ],
           ),
-        )
+        ),
+          TextField(
+            controller: _textEditingController,
+            onSubmitted: _onSubmitted,
+            enabled: true,
+            maxLength: 330, // 入力数
+            maxLengthEnforced: true, // 入力上限になったときに、文字入力を抑制するか
+            style: TextStyle(color: Colors.black,fontSize: 18),
+            obscureText: false,
+            minLines:15,
+            maxLines:15,
+            decoration: const InputDecoration(
+              hintText: '　投稿内容を記載してください',
+
+            ),
+          ),
+        ]
+    )
+      ),
     );
   }
 }
-
-
