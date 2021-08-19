688fdc5a171042cc6a8515ab75d1697b229f1f47
yoshiki20010524 <75286616+yoshiki20010524@users.noreply.github.com>
2021-08-19 20:45:41 +0900

    reply-functions-layouts
    リプライ機能のレイアウトの追加

--- lib/screens/content.dart
+++ lib/screens/content.dart
@@ -13,21 +13,67 @@ class ContentPage extends StatefulWidget {
 
 
 final _firestore = FirebaseFirestore.instance;
+TextEditingController _textEditingControllerReply = TextEditingController();
 class _ContentPagePageState extends State<ContentPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("投稿フォーム"),
-      ),
-        body: Card(
-    child: ListTile(
-
-        title: Text(widget.content),
-    //ここに名前かジャンルを入れる
-
+        title: Text("Q＆A"),
+          actions: <Widget>[
+      IconButton(
+      icon: Text("投稿"),
+      onPressed: () => setState(() {
+      }),
     ),
-        )
+    ]
+      ),
+      body: Stack(
+          children: [
+          Card(
+          child: ListTile(
+            subtitle: Text("  yoshiki"),
+              title: Text(widget.content),
+          //ここに名前かジャンルを入れる
+          ),
+              ),
+    SafeArea(child:Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children:[
+          TextFormField(
+            autofocus: true,
+            style: new TextStyle(
+              fontSize: 20.0,
+              color: Colors.black,
+
+            ),
+            controller: _textEditingControllerReply,
+            decoration: new InputDecoration(
+              border: OutlineInputBorder(),
+             hintText: ('回答する'),
+              prefixIcon: Icon(Icons.face),
+
+            ),
+          ),
+        ]
+    ) ,)
+
+          ]
+        ),
       );
   }
 }
+
+
+// return TextField(
+// autofocus: true,
+// style: new TextStyle(
+// fontSize: 20.0,
+// color: Colors.black,
+// ),
+// controller: _controller,
+// decoration: new InputDecoration(
+// border: InputBorder.none,
+// hintText: 'コメントを追加',
+// ),
+// ),
