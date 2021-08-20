57cd3367d1cacda868b6e78f941445663bc1a206
nanikasi <nanikasi1024@gmail.com>
2021-08-20 19:12:57 +0900

    設定画面と設定内容の追加
    main.dartにsettingView.dartへのnavigator.pushを追加、settingView.dartをListViewとCardを使って
    　・通知
    　・アカウント削除
    　・お問い合わせ
    　・このアプリについて
    を追加
    settingsフォルダ以下にアカウント削除以外のファイルを追加

---
+++ lib/screens/settings/inquiryView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class InquiryView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("お問い合わせ"),
+      ),
+      body: Center()
+    );
+  }
+}
