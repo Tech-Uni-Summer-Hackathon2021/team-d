d766ac2959a2759c9686d4e9c1ea670b8a446f46
さわしおり <74851351+sawa-4@users.noreply.github.com>
2021-08-28 15:24:16 +0900

    デフォルトのユーザーアイコン入れました
    入れただけ

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -7,9 +5,9 @@ import 'screens/routes/postView.dart';
 import 'screens/routes/userView.dart';
 import 'screens/settingView.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:sawa/screens/auth/auth.dart';
 import 'screens/auth/auth.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -17,33 +15,44 @@ Future<void> main() async {
 
   runApp(MyApp());
   //ログイン振り分け
-
 }
 
 class MyApp extends StatelessWidget {
   @override
-
+  static const MaterialColor white = const MaterialColor(
+    0xFFFFFFFF,
+    const <int, Color>{
+      50: const Color(0xFFFFFFFF),
+      100: const Color(0xFFFFFFFF),
+      200: const Color(0xFFFFFFFF),
+      300: const Color(0xFFFFFFFF),
+      400: const Color(0xFFFFFFFF),
+      500: const Color(0xFFFFFFFF),
+      600: const Color(0xFFFFFFFF),
+      700: const Color(0xFFFFFFFF),
+      800: const Color(0xFFFFFFFF),
+      900: const Color(0xFFFFFFFF),
+    },
+  );
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
         // This is the theme of your application.
-        primarySwatch: Colors.blue,
+        primarySwatch: white,
       ),
       home: MyHomePage(title: '大学生のための質問教室'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget{
+class MyHomePage extends StatefulWidget {
   MyHomePage({key, this.title}) : super(key: key);
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
@@ -55,18 +64,15 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var routes = [PostView(), UserView()];
 
-    var routes =[PostView(), UserView()];
-
-    return Scaffold (
-
+    return Scaffold(
       appBar: AppBar(
         title: Text('大学生のための質問教室'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.settings),
             onPressed: () {
-
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => SettingView()),
@@ -78,14 +84,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
       body: routes[_selectedIndex],
 
-
       bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        //投稿とユーザーで今選んでいる方のアイコンの色
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.article),
             title: Text('投稿'),
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.account_circle),
             title: Text('ユーザー'),
@@ -97,8 +103,5 @@ class _MyHomePageState extends State<MyHomePage> {
 
       // This trailing comma makes auto-formatting nicer for build methods.
     );
-
   }
-
 }
-
