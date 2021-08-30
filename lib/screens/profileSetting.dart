a131450531c30ebbf67ec2ced9ef734a314e13b8
yoshiki20010524 <75286616+yoshiki20010524@users.noreply.github.com>
2021-08-31 01:27:34 +0900

    修正と実機テストに基づいた修正
    実機テストでバックエンド処理が動くことを確認しました。

--- lib/screens/profileSetting.dart
+++ lib/screens/profileSetting.dart
@@ -18,8 +18,6 @@ final _firestore = FirebaseFirestore.instance;
 
 Future _getPreferences() async {
   var preferences = await SharedPreferences.getInstance();
-  // SharedPreferencesから値を取得.
-  // keyが存在しない場合はnullを返す.
   //documentIDの取得のために使用
   print(preferences.getString("test_string_key"));
 }
@@ -65,7 +63,6 @@ class _ProfileSetViewState extends State<ProfileSetView> {
     } else {
       return;
     }
-
     try {
       var task = await firebase_storage.FirebaseStorage.instance
           .ref('user_icon/' + widget.uid + '.jpg')
@@ -74,13 +71,15 @@ class _ProfileSetViewState extends State<ProfileSetView> {
       var preferences = await SharedPreferences.getInstance();
       task.ref.getDownloadURL().then((downloadURL) => FirebaseFirestore.instance
           .collection("user")
-          .doc(preferences.getString("test_string_key"))
+          .doc(preferences.getString("start"))
           .update({'avatar_image_path': downloadURL}));
     } catch (e) {
       print("Image upload failed");
       print(e);
     }
   }
+
+
 
   String user_name;
 
@@ -191,10 +190,10 @@ class _ProfileSetViewState extends State<ProfileSetView> {
                     );
                     _getPreferences();
                     var preferences = await SharedPreferences.getInstance();
-                    if (preferences.containsKey("test_string_key")) {
+
                       //更新
                       FirebaseFirestore.instance.collection('user')
-                          .doc(preferences.getString("test_string_key"))
+                          .doc(preferences.getString("start"))
                           .update(
                         {
                           "name": user_name,
@@ -203,31 +202,6 @@ class _ProfileSetViewState extends State<ProfileSetView> {
                           "gender": user_gender,
                         },
                       );
-                    }
-                    else {
-                      //追加
-                      var docRef = await _firestore.collection("user").add(
-                        {
-                          "name": user_name,
-                          "age": user_age,
-                          "major": user_major,
-                          "gender": user_gender,
-                          "uid": widget.uid,
-                        },
-                      );
-                      var documentId = docRef.id;
-                      _firestore.collection("user").doc(documentId).update(
-                        {
-                          "documentID": documentId
-                        },
-                      );
-                      Future _setPreferences() async {
-                        var preferences = await SharedPreferences.getInstance();
-                        // SharedPreferencesに値を設定
-                        preferences.setString("test_string_key", documentId);
-                      }
-                      _setPreferences();
-                    }
                   }
                 }
             )
@@ -276,12 +250,10 @@ class _ProfileSetViewState extends State<ProfileSetView> {
                               ),
                               SizedBox(height: 10.0,),
                               TextButton(onPressed: () async {
-                                // getImageFromGallery();
                                 showBottomSheet();
                               }, child: Text(
                                   'プロフィール画像を変更')),
                               TextFormField(
-
                                 controller: _nameController,
                                 onFieldSubmitted: (value) {
                                   print(value);
