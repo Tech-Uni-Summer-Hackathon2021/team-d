d05371d247cc0fd9965e2da8bcacc979c0e5b507
yoshiki20010524 <75286616+yoshiki20010524@users.noreply.github.com>
2021-09-02 04:58:27 +0900

    add-development

--- lib/screens/settingView.dart
+++ lib/screens/settingView.dart
@@ -4,74 +4,84 @@ import 'settings/notifyView.dart';
 import 'settings/inquiryView.dart';
 import 'settings/destroyUserView.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'dart:core';
 class SettingView extends StatelessWidget {
+
   @override
+  _launchURL() async {
+    const url = "https://forms.gle/KwX57QFeHqWKXBi87";
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not Launch $url';
+    }
+  }
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("設定画面"),
-      ),
-      body: Center(
-        child: ListView(
-          children: <Widget>[
-            Card(
-              child: InkWell(  // InkWellはCardの子ウィジェット
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => NotifyView()),
-                  );
-                },
-                child: ListTile(
-                  title: Text('通知について'),
-                ),
-              ),
-            ),
-
-            Card(
-              child: InkWell(  // InkWellはCardの子ウィジェット
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => DestroyUserView()),
-                  );
-                },
-                child: ListTile(
-                  title: Text('アカウント削除'),
-                ),
-              ),
-            ),
-
-            Card(
-              child: InkWell(  // InkWellはCardの子ウィジェット
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => InquiryView()),
-                  );
-                },
-                child: ListTile(
-                  title: Text('お問い合わせ'),
-                ),
-              ),
-            ),
-
-            Card(
-              child: InkWell(  // InkWellはCardの子ウィジェット
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AboutView()),
-                  );
-                },
-                child: ListTile(
-                  title: Text('このアプリについて'),
-                ),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("設定画面"),
         ),
-      )
+        body: Center(
+          child: ListView(
+            children: <Widget>[
+              Card(
+                child: InkWell(  // InkWellはCardの子ウィジェット
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NotifyView()),
+                    );
+                  },
+                  child: ListTile(
+                    title: Text('通知について'),
+                  ),
+                ),
+              ),
+
+              Card(
+                child: InkWell(  // InkWellはCardの子ウィジェット
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DestroyUserView()),
+                    );
+                  },
+                  child: ListTile(
+                    title: Text('アカウント削除'),
+                  ),
+                ),
+              ),
+
+              Card(
+                child: InkWell(  // InkWellはCardの子ウィジェット
+                  onTap: () {
+                    _launchURL();
+                  },
+                  child: ListTile(
+                    title: Text('お問い合わせ'),
+                  ),
+                ),
+              ),
+
+              Card(
+                child: InkWell(  // InkWellはCardの子ウィジェット
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => AboutView()),
+                    );
+                  },
+                  child: ListTile(
+                    title: Text('このアプリについて'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
     );
   }
 }
+
+
+
